28d113c9ff677b4c791009e0f7fbeaddc8a76992
Yared91 <ykebede010@gmail.com>
2023-09-21 04:29:01 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,7 +2,7 @@
 /**
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: the string to be cahnged
- * Retrurn: *str
+ * Return: *str
  */
 char *string_toupper(char *str)
 {
