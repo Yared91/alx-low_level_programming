01b8b214b6f1b88751b619d31ca05bbe33b63b2e
Yared91 <ykebede010@gmail.com>
2023-10-12 04:57:54 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_strings - Writing a function that prints strings
@@ -9,7 +9,7 @@
  * @...: input
  *
  * Description: if separator is NULL, it is not printed.
- * If one of the strings if NULL, (nil) is printed instead
+ * If one of the strings if NULL, (nil) is printed instead.
  */
 
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -19,16 +19,20 @@ char *str;
 unsigned int index;
 
 va_start(strings, n);
+
 for (index = 0; index < n; index++)
 {
 str = va_arg(strings, char *);
+
 if (str == NULL)
 printf("nil");
 else
 printf("%s", str);
+
 if (index != (n - 1) && separator != NULL)
 printf("%s", separator);
 }
 printf("\n");
+
 va_end(strings);
 }
