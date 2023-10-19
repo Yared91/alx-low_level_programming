c347ee954be9857b9c7ffb62b146259d48fe7f13
Yared91 <yaredkebede010@gmail.com>
2023-10-19 13:00:38 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,7 +10,7 @@
 
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+size_t element = 0;
 while (h)
 {
 if (h->str == NULL)
@@ -18,8 +18,8 @@ printf("[0] (nil)\n");
 
 else
 printf("[%d] %s\n", h->len, h->str);
-nodes++;
+element++;
 h = h->next;
 }
-return (nodes);
+return (element);
 }
