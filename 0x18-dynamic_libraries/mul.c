c86a312c0b37c0582401531a96d6e79bbbeb7c96
Yared91 <ykebede010@gmail.com>
2023-12-11 03:45:34 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * mul - multiply two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * sub - subtract two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: subtraction
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
