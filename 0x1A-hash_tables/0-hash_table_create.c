f89e35ced6e4e06d40599e946f7a899a492ac79c
Yared91 <ykebede010@gmail.com>
2023-12-21 21:02:59 -0800

    0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,23 +9,30 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_node_t **array;
+	unsigned long int i = 0;
+
+	if (size == 0)
+		return (NULL);
 
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	array = malloc(sizeof(*array) * size);
 
-	if (hash_table->array == NULL)
+	if (array == NULL)
 	{
 		free(hash_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
-		hash_table->array[i] = NULL;
+		array[i] = NULL;
+		i++;
 	}
+	hash_table->size = size;
+	hash_table->array = array;
+
 	return (hash_table);
 }
